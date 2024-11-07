94a350a24a82a62b78e37ead222ac25dc8a0aa0d
CobaltTarantula <zack.r.clifford@gmail.com>
2024-11-07 12:59:51 -0700

    route redo

--- src/app.jsx
+++ src/app.jsx
@@ -20,17 +20,17 @@ export default function App() {
         <header className='container-fluid'>
           <nav className="navbar navbar-dark bg-dark">
             <div className="container-fluid">
-              <NavLink className="navbar-brand" to="#">MorseDecoder</NavLink>
+              <NavLink className="navbar-brand" to="/">MorseDecoder</NavLink>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <NavLink className="nav-link active" aria-current="page" to="index.html">Home</NavLink>
+                    <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
                   </li>
                   <li className="nav-item dropdown">
-                    <NavLink className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <NavLink className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                       Play
                     </NavLink>
                     <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
