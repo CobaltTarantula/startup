1d83274234b5812fb42bdc54fce05fe95f09caae
CobaltTarantula <zack.r.clifford@gmail.com>
2024-11-07 21:42:48 -0700

    morseGames and scores jsx stuff

--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const scoreRows = [];
+if (scores.length) {
+  for (const [i, score] of scores.entries()) {
+    scoreRows.push(
+      <tr key={i}>
+        <td>{i}</td>
+        <td>{score.name.split('@')[0]}</td>
+        <td>{score.score}</td>
+        <td>{score.date}</td>
+      </tr>
+    );
+  }
+} else {
+  scoreRows.push(
+    <tr key='0'>
+      <td colSpan='4'>Be the first to score</td>
+    </tr>
+  );
+}
+
 export function Scores() {
   return (
     <main /*className="h-center"*/>
