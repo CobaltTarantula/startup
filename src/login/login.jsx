8ac39d57910a3ed03693488da554cda6839e1aef
CobaltTarantula <zack.r.clifford@gmail.com>
2024-11-07 22:14:40 -0700

    refactor files and components

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 
-export function Login() {
+// export function Login() {
+//   return (
+//     <main>
+//       <h1 className="h-center">Welcome to Morse Decoder</h1>
+//       <form method="get" action="play.html">
+//         <div>
+//           <span>@</span>
+//           <input id="email" type="text" placeholder="your@email.com" />
+//         </div>
+//         <div>
+//           <span>🔒</span>
+//           <input id="password" type="password" placeholder="password" />
+//         </div>
+//         <div>
+//           <button type="button" className="btn btn-primary btn-sm">Login</button>
+//           <button type="button" className="btn btn-primary btn-sm">Create</button>
+//         </div>
+//       </form>
+//     </main>
+//   );
+// }
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
-    <main>
-      <h1 className="h-center">Welcome to Morse Decoder</h1>
-      <form method="get" action="play.html">
-        <div>
-          <span>@</span>
-          <input id="email" type="text" placeholder="your@email.com" />
-        </div>
-        <div>
-          <span>🔒</span>
-          <input id="password" type="password" placeholder="password" />
-        </div>
-        <div>
-          <button type="button" className="btn btn-primary btn-sm">Login</button>
-          <button type="button" className="btn btn-primary btn-sm">Create</button>
-        </div>
-      </form>
+    <main className='container-fluid bg-secondary text-center'>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+        {authState === AuthState.Authenticated && <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
     </main>
   );
 }
