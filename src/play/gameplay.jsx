356d9fe58987d9c98f3757841c57f142f5fdd419
CobaltTarantula <zack.r.clifford@gmail.com>
2024-11-07 22:16:46 -0700

    uncomment files and paste needed from example

--- src/play/gameplay.jsx
+++ src/play/gameplay.jsx
@@ -1,11 +1,11 @@
-// import { Players } from './players';
-// import { MorseGame } from './morseGame';
+import { Players } from './players';
+import { MorseGame } from './morseGame';
 
-// export function Play(props) {
-//   return (
-//     <main className='bg-secondary'>
-//       <Players userName={props.userName} />
-//       <MorseGame userName={props.userName} />
-//     </main>
-//   );
-// }
+export function Play(props) {
+  return (
+    <main className='bg-secondary'>
+      <Players userName={props.userName} />
+      <MorseGame userName={props.userName} />
+    </main>
+  );
+}
