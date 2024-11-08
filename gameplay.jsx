599033c115981f38e1ec35c2566e4362bb79cfe8
CobaltTarantula <zack.r.clifford@gmail.com>
2024-11-07 21:29:28 -0700

    gameplay.jsx

---
+++ gameplay.jsx
@@ -0,0 +1,11 @@
+import { Players } from './players';
+import { MorseGame } from './morseGame';
+
+export function Play(props) {
+  return (
+    <main className='bg-secondary'>
+      <Players userName={props.userName} />
+      <MorseGame userName={props.userName} />
+    </main>
+  );
+}
