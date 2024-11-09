9089468a78664626ebe3d1cc63c429e7f9c1dd72
CobaltTarantula <zack.r.clifford@gmail.com>
2024-11-08 23:03:06 -0700

    pages link up but need to implement hidden pages

--- src/gamemode/gamemode.jsx
+++ src/gamemode/gamemode.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function Play() {
   return (
@@ -6,13 +7,13 @@ export function Play() {
       <br />
 
       <div>
-        <button type="button" className="btn btn-dark btn-lg">Code<br/>(English to Morse code)</button>
+        <Link to="/english-morse" className="btn btn-dark btn-lg">Code<br/>(English to Morse code)</Link>
       </div>
 
       <br />
       
       <div>
-        <button type="button" className="btn btn-outline-primary btn-lg">Decode<br/>(Morse code to English)</button>
+        <Link to="/morse-english" className="btn btn-outline-primary btn-lg">Decode<br/>(Morse code to English)</Link>
       </div>
 
       <br />
